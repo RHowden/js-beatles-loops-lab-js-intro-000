2820b4ee7c19614116f625f007e4700842fbc8f5
Robert Howden <robert.howden@current-rms.com>
2020-04-21 08:08:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-johnLennonFacts = (a) => {
+theBeatlesPlay = (m, ins) => {
+  var result = []
+  for (let i = 0; i < m.length; i++) {
+    result.push(`${m[i]} plays ${ins[i]}`)
+  }
+
+  return result
+}
+
+function johnLennonFacts(a) {
   var result = []
 
   let i = 0
   while (i < a.length) {
     result.push(a[i] + '!!!')
     i++
-  }
-
-  return result
-}
-
-theBeatlesPlay = (m, ins) => {
-  var result = []
-  for (let i = 0; i < m.length; i++) {
-    result.push(`${m[i]} plays ${ins[i]}`)
   }
 
   return result
