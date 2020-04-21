72aee11fe0844c09c85786b734aa231485fdde49
Robert Howden <robert.howden@current-rms.com>
2020-04-21 07:45:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 theBeatlesPlay = (m, ins) => {
   result = []
   for (let i = 0; i < m.length; i++) {
-    result.push(`${m[0]} plays ${ins[i]} `)
+    result.push(`${m[i]} plays ${ins[i]} `)
   }
 
   return result
